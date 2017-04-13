19029f61e926bd9ed49423f8256b2b22f82b49b8
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-13 15:54:15 -0400

    made get and post for add new movie in moviesControllers.

--- tiyMovieRental/tiyMovieRental/Controllers/MoviesController.cs
+++ tiyMovieRental/tiyMovieRental/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tiyMovieRental.Models;
 using tiyMovieRental.Services;
 
 namespace tiyMovieRental.Controllers
@@ -16,6 +17,31 @@ namespace tiyMovieRental.Controllers
             var movies = new MoviesServices().GetAllMovies();
             //pass them to the view
             return View(movies);
+        }
+
+        //this is my get for my putpost. 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //add new movie
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+
+            var newMovie = new Movies
+            {
+                Name = collection["Name"],
+                YearReleased = int.Parse(collection["YearReleased"]),
+                Director = collection["Director"],
+                GenreId = int.Parse(collection["GenreId"]),
+
+            };
+            // TODO: Put into db
+            new MoviesServices().AddMovie(newMovie);
+            return RedirectToAction("Index");
         }
     }
 }
