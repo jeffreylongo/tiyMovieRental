02bb8d83ef9525e16e23f66d3b8fe4a54c3a94ce
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-13 21:50:59 -0400

    created post for add new customer in customersController

--- tiyMovieRental/tiyMovieRental/Controllers/CustomersController.cs
+++ tiyMovieRental/tiyMovieRental/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using tiyMovieRental.Models;
 using tiyMovieRental.Services;
 
 namespace tiyMovieRental.Controllers
@@ -17,5 +18,29 @@ namespace tiyMovieRental.Controllers
             //pass them to the view
             return View(customers);
         }
+        
+        //this is my get for my putpost. 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //add new customer
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+
+            var newCustomer = new Customers
+            {
+                Name = collection["Name"],
+                PhoneNumber = int.Parse(collection["PhoneNumber"]),
+                Email = collection["Email"],
+
+            };
+            new CustomersServices().AddCustomer(newCustomer);
+            return RedirectToAction("Index");
+        }
     }
+
 }
