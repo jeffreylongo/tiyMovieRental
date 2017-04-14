e2a56a8d510613ffe579d776e91740f7b216935f
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-13 21:47:48 -0400

    made customers controller, made initial get for get all customers in customerscontroller

---
+++ tiyMovieRental/tiyMovieRental/Controllers/CustomersController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using tiyMovieRental.Services;
+
+namespace tiyMovieRental.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            //get all customers
+            var customers = new CustomersServices().GetAllCustomers();
+            //pass them to the view
+            return View(customers);
+        }
+    }
+}
