abfa44855ac0f6f469f8c50a750d23148c9a60bd
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-13 21:18:58 -0400

    made customers model

---
+++ tiyMovieRental/tiyMovieRental/Models/Customers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace tiyMovieRental.Models
+{
+    public class Customers
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int PhoneNumber { get; set; }
+        public string Email { get; set; }
+
+        public Customers(SqlDataReader reader)
+        {
+            this.Id = (int)reader["Id"];
+            this.Name = reader["Name"].ToString();
+            this.PhoneNumber = (int)reader["PhoneNumber"];
+            this.Email = reader["Email"].ToString();
+
+        }
+
+        public Customers()
+        {
+        }
+    }
+}
