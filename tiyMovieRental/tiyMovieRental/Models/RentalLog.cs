0efb9a360d4412d80e574a4a730be8164abb80aa
Jeff Longo <jeffreylongo@yahoo.com>
2017-04-15 17:49:12 -0400

    created rentallog model

---
+++ tiyMovieRental/tiyMovieRental/Models/RentalLog.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace tiyMovieRental.Models
+{
+    public class RentalLog
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public int MovieId { get; set; }
+        public DateTime DateCheckedOut { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public RentalLog(SqlDataReader reader)
+        {
+            this.Id = (int)reader["Id"];
+            this.CustomerId = (int)reader["CustomerId"];
+            this.MovieId = (int)reader["YearReleased"];
+            this.DateCheckedOut = (DateTime)reader["DateChekedOut"];
+            this.DueDate = (DateTime)reader["DueDate"];
+
+        }
+
+        public RentalLog()
+        {
+        }
+    }
+}
